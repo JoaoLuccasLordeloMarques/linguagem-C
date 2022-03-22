b12d9e8dcbfd1d2357b3f24e1a1b36725938f94e
João Luccas <101368682+Lordelo22@users.noreply.github.com>
2022-03-22 16:54:50 -0300

    Add files via upload

---
+++ variaveis.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+    //lordello22 
+    int MeuCarro; 
+    MeuCarro = 3;
+    printf ("eu tenho %i carros\n", MeuCarro); 
+
+    int KGnaAcademia; 
+    KGnaAcademia = 55;
+    printf ("eu peguei %ikg na academia\n", KGnaAcademia);
+
+    int jogosqueeucomprei; 
+    jogosqueeucomprei = 10;
+    printf ("eu comprei %i jogos na steam\n",jogosqueeucomprei); 
+
+    printf ("eu tenho %i carros, quando eu vou na academia eu pego %i kg e na minha ultima compra na steam eu comprei %i jogos", MeuCarro, KGnaAcademia, jogosqueeucomprei); 
+
+    return 0; 
+
+}    
