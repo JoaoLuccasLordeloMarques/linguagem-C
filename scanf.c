fd3ed4399bec7ec6d8d857bf6670db9efae43d28
João Luccas <101368682+Lordelo22@users.noreply.github.com>
2022-03-22 17:26:56 -0300

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+
+int main ()
+{
+    //Lordello22 
+
+    int suabasemaior; 
+    int suabasemenor;
+    int altura; 
+    int area; 
+
+    printf ("Digite sua base maior:\n");
+    scanf ("%i", &suabasemaior);
+
+    printf ("Digite sua base menor:\n");
+    scanf ("%i", &suabasemenor);
+
+    printf ("Digite sua altura:\n"); 
+    scanf ("%i", &altura); 
+
+    area = suabasemaior + suabasemenor * altura /2; 
+
+    printf ("Sua area e de %i", area);
+
+  return 0; 
+
+} 
+
