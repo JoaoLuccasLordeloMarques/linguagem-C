8ec999ff27731545c6fc8a4dbdb0373d148dcf95
João Luccas <101368682+Lordelo22@users.noreply.github.com>
2022-03-23 18:30:40 -0300

    Add files via upload

---
+++ floatEchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () 
+{
+
+    //Lordello22: Float e Char
+
+  int numero = 25; 
+  float decimal = 25.55;
+  char palavra = 'L'; 
+
+  printf ("O meu numero favorito e %i\n", numero);
+  printf ("Eu nao gosto de numero quebrado, tal como %f\n", decimal);
+  printf ("Eu acho a letra %c bonita\n", palavra);
+
+
+  return 0;
+
+}
