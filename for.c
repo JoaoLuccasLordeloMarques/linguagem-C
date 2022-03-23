8911b02b31337658c2a9376d7dc18ca315946769
João Luccas <101368682+Lordelo22@users.noreply.github.com>
2022-03-23 19:11:27 -0300

    Add files via upload

---
+++ for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{ 
+       
+    //Lordello22: For 
+
+       int contador;
+       int num1 = 1; 
+
+       for (contador = 1; contador <= 10; contador = contador + 1) {
+
+            printf ("multiplica\n");
+            }
+
+       return 0;
+
+}
