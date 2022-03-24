23960514a085538e22047648a7c559391b25abe6
João Luccas <101368682+Lordelo22@users.noreply.github.com>
2022-03-24 13:26:31 -0300

    Add files via upload

---
+++ whileDo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+     
+     int a = 1; 
+
+   while (a != 3) {
+       printf ("Proesperidade p nos\n");
+       ++a;
+    }
+
+   do {
+        printf ("Multiplica\n");
+   } while (a != 3); 
+   
+
+   return 0; 
+
+}
