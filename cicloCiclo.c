6ee1ff9a1157d625697937cb4f8f48b0c520b0ac
João Luccas <101368682+Lordelo22@users.noreply.github.com>
2022-04-06 15:37:31 -0300

    Add files via upload

---
+++ cicloCiclo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void) {
+
+for (int a = 1; a <= 5; ++a) {
+    printf ("**Escute Vampiro %i vezes**\n", a);
+for (int b = 1; b <= 5; ++b) {
+    printf ("Para assistir no youtube:www.youtube.com/watch?v=TKqa-ZXGKYA&ab_channel=30PRAUM\n");}
+for (int c = 1; c <= 5; ++c) {
+    printf ("Para escutar no Spotify open.spotify.com/track/6bTdZ7xfKp3NqqADJ8HLyj?si=a20e5c0f400a4c28\n");}
+    printf ("\n");
+}
+
+
+
+return 0;
+
+}
